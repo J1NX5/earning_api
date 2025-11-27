2ae85f4dfc3a96f477b844335a0d9e77d15fdd9c
j1nx5 <j1nx@posteo.de>
2025-11-27 19:51:30 +0100

    expant insert and table

--- db.py
+++ db.py
@@ -4,28 +4,12 @@ class DBManager:
     def __init__(self):
         self.db_path: str = "data.db"
         self.conn = self._create_connection(self.db_path)
-        self._create_table()
+        self.create_earning_report_table()
 
     def _create_connection(self, db_file):
         connection = sqlite3.connect(db_file)
         return connection
     
-    def _create_table(self):
-        cursor = self.conn.cursor()
-        cursor.execute('''
-            CREATE TABLE IF NOT EXISTS data (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                symbol TEXT NOT NULL,
-                name TEXT NOT NULL,
-                reportDate DATE,
-                fiscalDate DATE,
-                estimate REAL,
-                currency TEXT,
-                timeOfTheDay TEXT
-            );
-        ''')
-        return self.conn.commit()
-
     def create_earning_report_table(self):
         cursor = self.conn.cursor()
         cursor.execute('''
@@ -37,67 +21,41 @@ class DBManager:
                 eps_estimated REAL NULL,
                 revenue_actual INTEGER NULL,
                 revenue_estimated INTEGER NULL,
-                last_updated TEXT NOT NULL
+                last_updated TEXT NOT NULL,
+                call_date TEXT NOT NULL
                 );
         ''')
         return self.conn.commit()
     
-    def insert_earning_report(self, symbol, date, eps_actual, eps_estimated, revenue_actual, revenue_estimated, last_updated):
+    def insert_earning_report(self, symbol, date, eps_actual, eps_estimated, revenue_actual, revenue_estimated, last_updated, call_date):
         cursor = self.conn.cursor()
         cursor.execute('''
-        INSERT INTO earnings (
-            symbol,
-            date,
-            eps_actual,
-            eps_estimated,
-            revenue_actual,
-            revenue_estimated,
-            last_updated
-            ) VALUES ( ?, ?, ?, ?, ?, ?, ? );
-        ''', (symbol, date, eps_actual, eps_estimated, revenue_actual, revenue_estimated, last_updated))
+            INSERT INTO earnings (symbol, name, reportDate, fiscalDate, estimate, currency, timeOfTheDay, call_date)
+            SELECT ?, ?, ?, ?, ?, ?, ?, ?
+            WHERE NOT EXISTS (
+                SELECT 1 FROM earnings
+                WHERE e.symbol = ? AND e.name = ? AND e.reportDate = ? 
+                AND e.fiscalDate = ? AND e.estimate = ? AND e.currency = ? 
+                AND e.timeOfTheDay = ? AND e.call_date = ?
+            )
+            ''', (  symbol, name, reportDate, fiscalDate, estimate, currency, timeOfTheDay, call_date,
+                    symbol, name, reportDate, fiscalDate, estimate, currency, timeOfTheDay, call_date  )
+        ) 
         return self.conn.commit()
 
-    
-    def insert_data(self, symbol, name, reportDate, fiscalDate, estimate, currency, timeOfTheDay):
-        cursor = self.conn.cursor()
-        cursor.execute('''
-            INSERT INTO data (
-                symbol,
-                name,
-                reportDate,
-                fiscalDate,
-                estimate,
-                currency,
-                timeOfTheDay
-            ) VALUES (?, ?, ?, ?, ?, ?, ?);
-        ''', (symbol, name, reportDate, fiscalDate, estimate, currency, timeOfTheDay))
-        return self.conn.commit()
-    
-    def fetch_all(self):
-        cursor = self.conn.cursor()
-        cursor.execute("SELECT * FROM data;")
-        rows = cursor.fetchall()
-        return rows
-    
-    def find_by_symbol_name(self, symbol, name):
-        cursor = self.conn.cursor()
-        cursor.execute('''
-            SELECT * FROM data WHERE symbol = ? AND name = ? ORDER BY reportDate DESC LIMIT 1;
-        ''', (symbol, name))
-        return cursor.fetchone()
     
     def find_by_symbol(self, symbol):
         cursor = self.conn.cursor()
         cursor.execute('''
-            SELECT * FROM data WHERE symbol = ? ORDER BY reportDate DESC LIMIT 1;
+            SELECT * FROM data WHERE symbol = ?;
         ''', (symbol,))
         return cursor.fetchone()
     
-    def update_values(self, symbol, name, reportDate, fiscalDate, estimate, currency, timeOfTheDay):
-        cursor = self.conn.cursor()
-        cursor.execute('''
-            UPDATE data
-            SET reportDate = ?, fiscalDate = ?, estimate = ?, currency = ?, timeOfTheDay = ?
-            WHERE symbol = ? AND name = ?;
-        ''', (reportDate, fiscalDate, estimate, currency, timeOfTheDay, symbol, name))
-        return self.conn.commit()
+    # def update_values(self, symbol, name, reportDate, fiscalDate, estimate, currency, timeOfTheDay):
+    #     cursor = self.conn.cursor()
+    #     cursor.execute('''
+    #         UPDATE data
+    #         SET reportDate = ?, fiscalDate = ?, estimate = ?, currency = ?, timeOfTheDay = ?
+    #         WHERE symbol = ? AND name = ?;
+    #     ''', (reportDate, fiscalDate, estimate, currency, timeOfTheDay, symbol, name))
+    #     return self.conn.commit()
--- earning.py
+++ earning.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 import csv
 import requests
 from db import DBManager
+from datetime import datetime
 # import logging
 
 # logging.basicConfig(
@@ -22,6 +23,7 @@ class Collector:
         self.api_key = os.getenv('API_KEY')
         self.api_key_2 = os.getenv('API_KEY_2')
         self.dmo = DBManager()
+        self.current_date = datetime.today().strftime('%Y-%m-%d')
 
     def get_data_by_symbol(self,symb: str):
         fetch_data = self.dmo.find_by_symbol(symb)
@@ -29,20 +31,23 @@ class Collector:
         print(f'search for data from symbol: {symb}')
         return fetch_data
         
-
+# This function call all symbols which are in earning
     def get_earning(self, len_param: int):
         url = f'https://financialmodelingprep.com/stable/earnings-calendar?apikey={self.api_key_2}'
         with requests.Session() as s:
             e_data = s.get(url).json()
+            # For each symbol we want to call the report
             for d in range(0,len(e_data)):
+                print(self.current_date)
                 try:
                     self.get_earning_report(e_data[d]['symbol'])
                 except Exception as e:
                     pass
                     print(e)
 
+# This function collect much data it os possible
     def get_earning_report(self, symb: str):
-        url = f'https://financialmodelingprep.com/stable/earnings?symbol={symb}&limit=4&apikey={self.api_key_2}'
+        url = f'https://financialmodelingprep.com/stable/earnings?symbol={symb}&limit=3&apikey={self.api_key_2}'
         with requests.Session() as s:
             self.dmo.create_earning_report_table()
             data = s.get(url).json()
@@ -54,5 +59,6 @@ class Collector:
 
 if __name__ == '__main__':
     clltr = Collector()
+    # For single run by execute: python earning.py
     clltr.get_earning(3)
     # clltr.get_earning_report('AAPL')
