68c7648e9cd6ddb86d544fdd6abe9e7ba03452aa
j1nx5 <j1nx@posteo.de>
2025-12-01 11:15:49 +0100

    add call earning

--- bavest_api.py
+++ bavest_api.py
@@ -6,19 +6,40 @@ class BavestCollector:
 
     def __init__(self):
         load_dotenv()
-        self.url = "https://api.bavest.co/v0/list/symbols"
-        self.payload = { "page": 1 }
-        self.headers = {
+        
+
+    def call_symbols(self):
+        url = "https://api.bavest.co/v0/list/symbols"
+        payload = { "page": 1 }
+        headers = {
             "accept": "application/json",
             "content-type": "application/json",
             "x-api-key": os.getenv('BAVEST_KEY')
         }
-
-    def call_symbols(self):
-        response = requests.post(self.url, json=self.payload, headers=self.headers)
+        response = requests.post(url, json=payload, headers=headers)
         print(response.text)
+
+
+    # get error maybe error on api side or i need a special token
+    def call_earning_confirmed(self):
+        url = "https://api.bavest.co/v0/calender/earnings_confirmed"
+
+        payload = {
+            "from": 1761988093,
+            "to": 1764580093,
+            "event": "earnings"
+        }
+        headers = {
+            "accept": "application/json",
+            "content-type": "application/json",
+            "x-api-key":os.getenv('BAVEST_KEY')
+        }
+
+        response = requests.post(url, json=payload, headers=headers)
+        print(response)
     
 
 if __name__ == '__main__':
     bco = BavestCollector()
-    bco.call_symbols()
+    # bco.call_symbols()
+    bco.call_earning_confirmed()
