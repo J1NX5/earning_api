5603ccd74120a0e24e83aa3e18c9ca729bd31c87
j1nx5 <j1nx@posteo.de>
2025-12-02 12:11:08 +0100

    splitt collect data from api

--- lib/api.py
+++
@@ -1,6 +0,0 @@
-from lib.db import DBManager
-
-class Api:
-
-    def __init__(self):
-        pass
--- main.py
+++ main.py
@@ -1,20 +1,11 @@
 # from typing import Union
 from fastapi import FastAPI
-from lib.api import Api
-from lib.watch import Watch
-from lib.jobcenter import Jobcenter
-
-app = FastAPI()
-jc = Jobcenter()
-jc.start()
 
 # Is actual not implemented
 @app.get("/api/{symbol}")
 def read_root(symbol):
-    _api = Api()
     return {"data": data}
 
 @app.get("/watch/{symbol}")
 def read_root(symbol):
-    _watch = Watch()
     return {"data": data}
